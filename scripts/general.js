8a294356544fcb916c2241ea52fff37efc1e1f79
Alessandro Paolantonio <alexpaolantonio@gmail.com>
2024-06-11 19:38:41 +0200

    aggiornamento base documento + base navbar e footer

---
+++ scripts/general.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+
+        // Gestione dello stato della navbar
+        var navbar = $('.navbar');
+        var logoNero = $('#logo-nero');
+        var logoBianco = $('#logo-bianco');
+
+        if (scroll > 150) {
+            if (!navbar.hasClass('scrolled')) {
+                navbar.addClass('scrolled');
+                logoNero.hide();
+                logoBianco.show();
+            }
+        } else {
+            if (navbar.hasClass('scrolled')) {
+                navbar.removeClass('scrolled nascondi');
+                logoNero.show();
+                logoBianco.hide();
+            }
+        }
+
+        if (scroll > 350) {
+            if (!navbar.hasClass('mostra')) {
+                navbar.addClass('mostra');
+            }
+        } else {
+            if (navbar.hasClass('mostra')) {
+                navbar.removeClass('mostra');
+                navbar.addClass('nascondi');
+            }
+        }
+    });
+});
