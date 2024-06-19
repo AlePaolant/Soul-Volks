8ad470523c6ca419db7c212c43fa8e13a9b3b074
AlePaolant <paolantonio.2000159@studenti.uniroma1.it>
2024-06-19 18:50:25 +0200

    aggiunte icone android e iphone, modificata la home, aggiunto font

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-    // Add any necessary jQuery functionality here
-    $('.btn-primary').click(function() {
-        alert('View Gallery button clicked!');
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    const letters = document.querySelectorAll('.letter');
+    const delay = 2000; // 2 secondi di ritardo prima di iniziare l'animazione
+    const fillDelay = 300; // Ritardo tra il riempimento di ogni lettera
+
+    function fillLetters() {
+        letters.forEach((letter, index) => {
+            setTimeout(() => {
+                letter.style.color = '#333';
+            }, index * fillDelay);
+        });
+    }
+
+    setTimeout(fillLetters, delay);
 });
