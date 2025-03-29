4d13fd7f5cd901bb5d7fff5e3b8d4edaff64047b
Alessandro Paolantonio <alexpaolantonio@gmail.com>
2025-03-29 19:15:57 +0100

    aggiornati collegamenti footer, creata pagina mvc25 con parallasse (ancora non responsive), aggiornata pagina eventi

---
+++ scripts/mvc25.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    $(window).on("scroll", function() {
+        var scrollTop = $(window).scrollTop();
+        
+        // valore alto = velocità alta - valore basso = velocità bassa
+        $(".background").css("top", scrollTop * 0.8 + "px");
+        $(".clouds-background").css("top", scrollTop * 0.7 + "px");
+        $(".mountains").css("top", scrollTop * 0.8 + "px");
+        $(".ground").css("top", scrollTop * 0.4 + "px");
+        $(".trees-sx").css("top", scrollTop * 0.4 + "px");
+        $(".foreground").css("top", scrollTop * 0.01 + "px"); 
+        $(".t1").css("top", scrollTop * 0.05 + "px");
+        $(".trees-dx").css("top", scrollTop * 0.3 + "px");
+        $(".cloud-front-1").css("top", scrollTop * 0.3 + "px");
+        $(".cloud-front-2").css("top", scrollTop * 0.35 + "px");
+        $(".cloud-front-3").css("top", scrollTop * 0.25 + "px");
+        $(".cloud-front-4").css("top", scrollTop * 0.3 + "px");
+        $(".mongolfiera").css("top", scrollTop * 0.6 + "px");
+        $(".main-title").css("top", scrollTop * 0.8 + "px");
+    });
+});
